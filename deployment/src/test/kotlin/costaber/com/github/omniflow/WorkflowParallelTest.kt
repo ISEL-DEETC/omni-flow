67d647c609c47b8450606b9e141a1fed6536f2e7
Tiago Silva <tiagosik1994@gmail.com>
2025-04-21 18:52:49 +0100

    feat: add WorkflowParallelTest.kt that gets render to "valid" google workflow

--- deployment/src/test/kotlin/costaber/com/github/omniflow/WorkflowParallelTest.kt
+++ deployment/src/test/kotlin/costaber/com/github/omniflow/WorkflowParallelTest.kt
@@ -1,7 +1,16 @@
 package costaber.com.github.omniflow
 
+import costaber.com.github.omniflow.cloud.provider.amazon.deployer.AmazonCloudDeployer
+import costaber.com.github.omniflow.cloud.provider.amazon.renderer.AmazonRenderingContext
+import costaber.com.github.omniflow.cloud.provider.google.deployer.GoogleCloudDeployer
+import costaber.com.github.omniflow.cloud.provider.google.renderer.GoogleTermContext
 import costaber.com.github.omniflow.dsl.*
 import costaber.com.github.omniflow.model.HttpMethod.GET
+import costaber.com.github.omniflow.renderer.IndentedRenderingContext
+import costaber.com.github.omniflow.resource.util.joinToStringNewLines
+import costaber.com.github.omniflow.traversor.DepthFirstNodeVisitorTraversor
+import costaber.com.github.omniflow.visitor.NodeContextVisitor
+import org.junit.Test
 import java.util.*
 
 internal class WorkflowParallelTest {
@@ -11,6 +20,17 @@ internal class WorkflowParallelTest {
         description("Calculator example")
         steps(
             step {
+                name("ListAssign")
+                description("Initialize variables")
+                context(
+                    assign {
+                        variable("listString" equal listOf("a", "b"))
+                        variable("listNumber" equal listOf(1,2))
+                        variable("c" equal "true")
+                    }
+                )
+            },
+            step {
                 name("Parallel")
                 description("Initialize variables")
                 context(
@@ -18,22 +38,82 @@ internal class WorkflowParallelTest {
                         // next("") // can implicit assume next step to be the next to run
                         branches(
                             // not sure how to share data between branches
-                            steps(
-                                step {}
-                            ),
-                            steps(
-                                step {}
-                            )
+                            branch {
+                                //# each branch missing a steps
+                                name("branch1")
+                                steps(
+                                    step {
+                                        name("Assign step")
+                                        description("Initialize variables")
+                                        assign {
+                                            context(
+                                                variable("Hello"  equal "Hello")
+                                            )
+                                        }
+                                    }
+                                )
+                            },
+                            branch {
+                                name("branch2")
+                                steps(
+                                    step {
+                                        name("Assign step 2")
+                                        description("Initialize variables")
+                                        assign {
+                                            context(
+                                                variable("Hello"  equal "Hello")
+                                            )
+                                        }
+                                    }
+                                )
+                            }
                         )
                     }
                 )
             },
             step {
-                name("ListAssign")
+                name("Parallel Iteration")
                 description("Initialize variables")
                 context(
-                    assign {
-                        variable("listString" equal listOf("a", "b"))
+                    parallel {
+                        // for loop unrolling might be a solution for aws, gcp already supports suporta
+                        // loop unrolling
+                        iteration {
+                            value("key")
+                            forEach(variable("listString")) // range(1, 9)
+                            //loop listas e chaves de um hashmap // for map goes through keys
+                            steps(
+                                step {
+                                    name("AssignParallelIteration")
+                                    description("Initialize variables")
+                                    context(
+                                        assign {
+                                            variable("d" equal variable("key"))
+                                        }
+                                    )
+                                }
+                            )
+                        }
+                        // next("") // can implicit assume next step to be the next to run
+//                        branches(
+//                            // not sure how to share data between branches
+//                            steps(
+//                                step {
+//                                    assign {
+//                                        context(
+//                                            variable("Hello"  equal "Hello")
+//                                        )
+//                                    }
+//                                }
+//                            ),
+//                            steps(
+//                                step {
+//                                    assign {
+//                                        variable("World" equal "Hello")
+//                                    }
+//                                }
+//                            )
+//                        )
                     }
                 )
             },
@@ -41,11 +121,22 @@ internal class WorkflowParallelTest {
                 name("For")
                 description("For example")
                 context(
-                    loop {
-                        value("value")
-                        range(1, 9) or forEach(variable("listString"))
-                        // for map goes through keys
-                        steps()
+                    iteration {
+                        value("key")
+                        range(1, 9) //|| forEach(variable("listString"))
+                        // lists and keys of hashmap // forEach in hashmap goes through keys
+                        // TODO default does what in iteration??
+                        steps(
+                            step {
+                                name("AssignIteration")
+                                description("Initialize variables")
+                                context(
+                                    assign {
+                                        variable("number" equal variable("key"))
+                                    }
+                                )
+                            }
+                        )
                     }
                 )
             },
@@ -124,5 +215,27 @@ internal class WorkflowParallelTest {
             }
         )
         result("divResult")
+    }
+
+
+    @Test
+    fun `test google full deployment`() {
+        val nodeTraversor = DepthFirstNodeVisitorTraversor()
+        val contextVisitor = NodeContextVisitor(GoogleCloudDeployer.Builder().createNodeRendererStrategyDecider())
+        val renderingContext = IndentedRenderingContext(termContext = GoogleTermContext())
+        val content = nodeTraversor.traverse(contextVisitor, generalWorkflow, renderingContext)
+            .filterNot(String::isEmpty)
+            .joinToStringNewLines()
+        println(content)
+    }
+
+    @Test
+    fun `test amazon full deployment`() {
+        val nodeTraversor = DepthFirstNodeVisitorTraversor()
+        val contextVisitor = NodeContextVisitor(AmazonCloudDeployer.Builder().createNodeRendererStrategyDecider())
+        val content = nodeTraversor.traverse(contextVisitor, generalWorkflow, AmazonRenderingContext())
+            .filterNot(String::isEmpty)
+            .joinToStringNewLines()
+        println(content)
     }
 }
